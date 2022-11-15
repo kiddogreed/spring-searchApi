552f0075a535887ff566dfeaf37d286e041a4e2c
Domingo John Ruselle R – MBTC Contractual <t-jrdomingo@mbtc.mgc.local>
2022-11-15 17:39:40 +0800

    revert mistakes

---
+++ src/main/java/net/javaguides/springboot/Controller/ProductController.java
@@ -0,0 +1,34 @@
+package net.javaguides.springboot.Controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.javaguides.springboot.Entity.Product;
+import net.javaguides.springboot.Services.ProductService;
+
+@RestController
+@RequestMapping("api/v1/products")
+public class ProductController {
+
+  private ProductService productService;
+
+  public ProductController(ProductService productService) {
+    this.productService = productService;
+  }
+  
+  @GetMapping("/search")
+  public ResponseEntity<List<Product>> searchProducts(@RequestParam("query") String query){
+    return ResponseEntity.ok(productService.searchProducts(query));
+  }
+
+
+  public Product createProduct(@RequestBody Product product){
+    return productService.createProduct(product);
+  }
+}
---
+++ src/main/java/net/javaguides/springboot/Repository/TestProdRepository.java
@@ -0,0 +1,15 @@
+package net.javaguides.springboot.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
+import net.javaguides.springboot.Entity.TestProd;
+
+public interface TestProdRepository extends JpaRepository<TestProd, Long> {
+  
+  @Query("SELECT t from Testprod t where "+
+  "t.name LIKE CONCAT('%',:query,'%')"+
+  "OR t.desc LIKE CONCAT('%',:query,'%')")
+  
+  List<TestProd> searchProducts(String query);
+}
